1ace0676206a9520cf8115978df00a5d09571470
darinmilner <darin.milner@gmail.com>
2024-01-26 19:42:36 +0700

    API gateway terraform code added

---
+++ terraform/lambdaInfrastructure/api-gateway.tf
@@ -0,0 +1,45 @@
+resource "aws_api_gateway_rest_api" "test-api" {
+  name = "${var.environment}-${local.short_region}-api"
+  description = "API Gateway"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "root-endpoint" {
+  rest_api_id = local.api_id
+  parent_id = aws_api_gateway_rest_api.test-api.root_resource_id
+  path_part = "api"   // endpoint  <URL>/api
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = local.api_id
+  resource_id = local.resource_id
+  http_method = local.http_post_type
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda-integration" {
+  rest_api_id = local.api_id
+  resource_id = local.resource_id
+  http_method = local.http_post_type
+  integration_http_method = aws_api_gateway_method.proxy.http_method
+  type = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "ok-response" {
+  rest_api_id = local.api_id
+  resource_id = local.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = local.ok_response
+}
+
+resource "aws_api_gateway_integration_response" "response-integration" {
+  rest_api_id = local.api_id
+  resource_id = local.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = local.ok_response
+
+  depends_on = [ aws_api_gateway_integration.lambda-integration, aws_api_gateway_method.proxy ]
+}
--- terraform/lambdaInfrastructure/main.tf
+++ terraform/lambdaInfrastructure/main.tf
@@ -14,4 +14,8 @@ provider "random" {}
 
 locals {
   short_region = replace(var.region, "-", "")
+  api_id = aws_api_gateway_rest_api.test-api.id 
+  resource_id = aws_api_gateway_resource.root-endpoint.id
+  http_post_type = "POST"
+  ok_response = "200"
 }
