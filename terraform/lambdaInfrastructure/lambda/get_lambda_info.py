20c5c7bce9015ff466b8bf466642e99adf606e72
darinmilner <darin.milner@gmail.com>
2024-04-29 18:24:29 +0700

    Lambda info py script updated

--- terraform/lambdaInfrastructure/lambda/get_lambda_info.py
+++ terraform/lambdaInfrastructure/lambda/get_lambda_info.py
@@ -5,27 +5,60 @@ client = boto3.client("lambda")
 region = "us-east-1"
 function_name = "Lambda1"
 
-response = client.get_function(
-    FunctionName=function_name,
-)
 
-last_modified = response["Configuration"]["LastModified"]
-handler = response["Configuration"]["Handler"]
-logging_config = response["Configuration"]["LoggingConfig"]
-# layers = response["Configuration"]["Layers"]
-try:
-    vpc_config = response["Configuration"]["VpcConfig"]
-    print(vpc_config)
-except:
-    print("The lambda does not have a Vpc Config")
 
-print(response)
-print(handler)
-print(logging_config)
-# print(layers)
+def get_lambda_info():
+    response = client.get_function(
+        FunctionName=function_name,
+    )
+    last_modified = response["Configuration"]["LastModified"]
+    handler = response["Configuration"]["Handler"]
+    logging_config = response["Configuration"]["LoggingConfig"]
+    layers = None 
+
+    try:
+        layers = response["Configuration"]["Layers"]
+    except KeyError:
+        print("The lambda does not have any layers attached.")
+
+    try:
+        vpc_config = response["Configuration"]["VpcConfig"]
+        print(vpc_config)
+    except KeyError:
+        print("The lambda does not have a Vpc Config")
+
+    print(response)
+    print(handler)
+    print(logging_config)
+    print(layers)
+
+    try:
+        queue = response["Configuration"]["DeadLetterConfig"]
+    except KeyError:
+        print("The lambda has no queue config.")
 
 def get_all_functions():
-    functions = client.list_functions()
-    print(f"Lambda functions ${functions}")
+    paginator = client.get_paginator("list_functions")
+
+    res = paginator.paginate()
+    all_functions = []
+    for r in res:
+        functions = r["Functions"]
     
-get_all_functions()
+        for function in functions:
+            function_name = function["FunctionName"]
+            all_functions.append(function_name)
+
+    return all_functions
+    
+all_lambdas =  get_all_functions()
+print(all_lambdas)
+
+#TODO loop through the lambda names and call the info function for each lambda
+
+ #aws lambda list-functions --max-items=10000 | jq -r '.Functions' | jq length
+ 
+ 
+
+ 
+ 
