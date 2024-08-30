0c7156246515f3bf23899ebdaabd83fdac8f8450
darinmilner <darin.milner@gmail.com>
2024-08-30 19:12:17 +0700

    ALB security group updated

--- terraform/scim-api/alb/security-group.tf
+++ terraform/scim-api/alb/security-group.tf
@@ -1,10 +1,18 @@
 resource "aws_security_group" "alb-sg" {
-  name        = "portfolio-sg"
+  name        = "alb-sg"
   description = "Allow TLS inbound and outbound traffic"
   vpc_id      = var.vpc-id
 
   ingress {
     description = "HTTP from ALB"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Traffic from ALB"
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
